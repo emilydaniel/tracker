f7ad875fc7e188355de415d29ad76517e0ccd8c4
Emily D <emdaniel@ccs.neu.edu>
2018-02-21 01:53:49 -0500

    Mostly working

---
+++ tracker/deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#adapted from memory deploy script
+export PORT=5100
+export MIX_ENV=prod
+export GIT_PATH=/home/tracker/src/tracker
+
+PWD=`pwd`
+if [ $PWD != $GIT_PATH ]; then
+    echo "Error: Must check out git repo to $GIT_PATH"
+    echo " Current directory is $PWD"
+    exit 1
+
+fi
+
+if [ $USER != "tracker" ]; then
+    echo "Error: Must run as user 'tracker'"
+    echo " Current user is $USER"
+    exit 2
+fi
+
+mix deps.get
+(cd assets && npm install)
+(cd assets && ./node_modules/brunch/bin/brunch b -p)
+mix phx.digest
+mix release --env=prod
+
+mkdir -p ~/www
+mkdir -p ~/old
+
+NOW=`date +%`
+if [ -d ~/www/tracker ]; then
+    echo mv ~/www/tracker ~/old/$NOW
+    mv ~/www/tracker ~/old/$NOW
+fi
+
+mkdir -p ~/www/tracker
+REL_TAR=~/src/tracker/_build/prod/rel/tracker/releases/0.0.1/tracker.tar.gz
+(cd ~/www/tracker && tar xzvf $REL_TAR)
+
+crontab - <<CRONTAB
+@reboot bash /home/tracker/src/memory/start.sh
+CRONTAB
+
+#. start.sh
--- tracker/lib/tracker_web/controllers/page_controller.ex
+++ tracker/lib/tracker_web/controllers/page_controller.ex
@@ -8,7 +8,12 @@ defmodule TrackerWeb.PageController do
   end
 
   def taskview(conn, _params) do
+    user_id = get_session(conn, :user_id)
+    curr_user = Tracker.Accounts.get_user(user_id)
     tasks = TaskList.list_tasks()
+            |> Enum.filter(fn(task) ->
+                 task.user.name == curr_user.name
+               end)
     changeset = TaskList.change_task(%TaskList.Task{})
     render conn, "taskview.html", tasks: tasks, changeset: changeset
   end
